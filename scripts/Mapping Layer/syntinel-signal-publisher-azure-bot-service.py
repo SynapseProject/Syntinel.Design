f14ad964a99d74d92d54e03ec41f59f7ead48fed
Guy Waguespack <guy.waguespack@gmail.com>
2019-02-06 14:05:11 -0600

    - Added updated PNG file for Techincal Architecture. - Updated AzureBotService Publisher To Create MSTeams Forms.

--- scripts/Mapping Layer/syntinel-signal-publisher-azure-bot-service.py
+++ scripts/Mapping Layer/syntinel-signal-publisher-azure-bot-service.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
         token = channel.get('config', {}).get('bearerToken')
         target = channel.get('target')
         conversationId = getConversationId(token)
-        body = CreateSlackMessage(signalId, signal, target)
+        body = CreateAzureBotMessage(signalId, signal, target)
         botReply = sendMessage(conversationId, token, body)
 
     reply = {
@@ -57,31 +57,54 @@ def sendMessage(conversationId, token, body):
 
     return content
     
-def CreateSlackMessage(signalId, signal, recipient):
-    mainTitle = signal.get('name', "Alert") + " : " + signal.get('description')
-    
+def CreateAzureBotMessage(signalId, signal, recipient):
     message = {
         "type": "message",
         "from": {
             "id": signalId
         },
         "text": "notify " + recipient,
-        "value": {
-            "text": mainTitle,
-            "attachments": [],
-            "valueType": "application/json"
-        }
+        "valueType": "application/json"
     }
-    
-    cues = signal.get('cues', {})
-    attachments = message.get('value').get('attachments')
-    for cue in cues:
-        attachment = CreateAttachment(signalId, cue, cues.get(cue))
-        attachments.append(attachment)
+
+    targetArray = recipient.partition("@")
+    if len(targetArray) == 3:
+        channelType = targetArray[2].lower()
+    else:
+        raise Exception("Unable To Determine Channel Type For Azure Bot Service Recipient [" + recipient + "].") 
+
+    if channelType == 'slack':
+        value = CreateSlackMessage(signalId, signal, recipient)
+    elif channelType == 'msteams':
+        value = CreateTeamsMessage(signalId, signal, recipient)
+    else:
+        raise Exception("Unknown Channel Type [" + channelType + "].")
+
+    message.update( { 'value': value } )
 
     return message
     
-def CreateAttachment(signalId, cueId, cue):
+#########################################
+# Slack Methods
+#########################################
+
+def CreateSlackMessage(signalId, signal, recipient):
+    mainTitle = signal.get('name', "Alert") + " : " + signal.get('description')
+    
+    value = {
+        "text": mainTitle,
+        "attachments": []
+    }
+    
+    cues = signal.get('cues', {})
+    attachments = value.get('attachments')
+    for cue in cues:
+        attachment = CreateSlackAttachment(signalId, cue, cues.get(cue))
+        attachments.append(attachment)
+
+    return value
+    
+def CreateSlackAttachment(signalId, cueId, cue):
     cueTitle = cue.get('name', "Alert") + " : " + cue.get('description')
     
     attachment = {
@@ -92,12 +115,13 @@ def CreateAttachment(signalId, cueId, cue):
     }
     
     for action in cue.get('actions'):
-        attAction = CreateAction(action)
-        attachment.get('actions').append(attAction)
+        attAction = CreateSlackAction(action)
+        if attAction:
+            attachment.get('actions').append(attAction)
     
     return attachment
-    
-def CreateAction(action):
+
+def CreateSlackAction(action):
     newAction = {}
     
     name = action.get('name')
@@ -124,3 +148,146 @@ def CreateAction(action):
         newAction.update( { 'value': defaultValue } )
 
     return newAction
+    
+#########################################
+# Microsoft Teams Methods
+#########################################
+    
+def CreateTeamsMessage(signalId, signal, recipient):
+    mainTitle = signal.get('name', "Alert") + " : " + signal.get('description')
+    
+    value = {
+        "type": "AdaptiveCard",
+        "body": [
+            {
+                "type": "Container",
+                "separator": False,
+                "items": [
+                    {
+                        "type": "TextBlock",
+                        "size": "Medium",
+                        "weight": "Bolder",
+                        "text": signal.get('name', "Syntinel Signal Received.")
+                    },
+                    {
+                        "type": "TextBlock",
+                        "size": "Medium",
+                        "text": signal.get('description')
+                    }
+                ]
+            }
+        ],
+        "actions": [],
+        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+        "version": "1.0",
+        "fallbackText": "Version Not Supported."
+    }
+    
+    cues = signal.get('cues', {})
+    actions = value.get('actions')
+    for cue in cues:
+        card = CreateTeamsCard(signalId, cue, cues.get(cue))
+        if len(cues) == 1:
+            # Create Single Cue In Main Body of Message
+            cardBody = card.get('body', [])
+            for cardBodyItem in cardBody:
+                value.get('body').append(cardBodyItem)
+                
+            cardActions = card.get('actions', [])
+            for cardActionItem in cardActions:
+                value.get('actions').append(cardActionItem)
+        else:
+            # Create Each Cue As An Action.ShowCard Button
+            newAction = {
+                "type": "Action.ShowCard",
+                "title": cue,
+                "card": card
+            }
+            value.get('actions').append(newAction)
+
+    return value
+    
+def CreateTeamsCard(signalId, cueId, cue):
+    card = {
+        "type": "AdaptiveCard",
+        "body": [
+            {
+                "type": "Container",
+                "separator": True,
+                "items": [
+                    {
+                        "type": "TextBlock",
+                        "size": "Medium",
+                        "weight": "Bolder",
+                        "text": cue.get('name'),
+                        "wrap": True
+                    },
+                    {
+                        "type": "TextBlock",
+                        "size": "Medium",
+                        "text": cue.get('description'),
+                        "wrap": True
+                    }
+                ]
+            }
+        ],
+        "actions": []
+    }
+    
+    actions = cue.get('actions', {})
+    for action in actions:
+        type = action.get('type')
+        if type == "button":
+            myAction = {
+                "type": "Action.Submit",
+                "id": "action",
+                "title": action.get('name'),
+                "data": {
+                    "callback_id": signalId + "|" + cueId,
+                    "action": action.get('defaultValue')
+                }
+            }
+            card.get('actions').append(myAction)
+        elif type == "choice":
+            myAction = {
+                "type": "Action.ShowCard",
+                "title": action.get('name'),
+                "card": {
+                    "type": "AdaptiveCard",
+                    "body": [],
+                    "actions": [
+                        {
+                            "type": "Action.Submit",
+                            "title": "Submit",
+                            "data": {
+                                "callback_id": signalId + "|" + cueId
+                            }
+                        }
+                    ]
+                }
+            }
+
+            myBody = {
+                "type": "Container",
+                "separator": True,
+                "items": [
+                    {
+                        "type": "Input.ChoiceSet",
+                        "id": "action",
+                        "choices": []
+                    }
+                ]
+            }
+            
+            for choice in action.get('values', []):
+                for kvp in choice:
+                    myChoice = {
+                        "title": choice.get(kvp),
+                        "value": kvp
+                    }
+                myBody.get('items')[0].get('choices').append(myChoice)
+
+            myAction.get('card').get('body').append(myBody)
+            card.get('actions').append(myAction)
+            
+    return card
